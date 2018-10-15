dba4ddbc17c97976d78ea9fd782cff78bd75542b
Andrei-Laurentiu Apostol <andreiapostol@andreilntiusmbp.home>
2018-10-15 20:05:33 +0100

    Made stencilPadding stencil

--- stencil.c
+++ stencil.c
@@ -6,8 +6,8 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void newStencil(const int nx, const int ny, double* restrict image, double* restrict currentRow);
-void stencil(const int nx, const int ny, double *  image, double *  tmp_image);
+void newStencil(const int nx, const int ny, double* image, double* restrict currentRow);
+void stencil(const int nx, const int ny, double *  image, double * restrict  tmp_image);
 void init_image(const int nx, const int ny, double * image);
 void output_image(const char * file_name, const int nx, const int ny, double *image);
 double wtime(void);
@@ -26,15 +26,15 @@ int main(int argc, char *argv[]) {
   int niters = atoi(argv[3]);
 
   // Allocate the image
-  double *image = malloc(sizeof(double)*nx*ny);
-  double *currentRow = malloc(sizeof(double)*ny);
+  double *image = malloc(sizeof(double)*(nx+2)*(ny+2));
+  double *currentRow = malloc(sizeof(double)*(ny+2));
 
-  for(int i = 0; i < ny; i++){
+  for(int i = 0; i < ny+2; i++){
     currentRow[i] = 0;
   }
 
   // Set the input image
-  init_image(nx, ny, image);
+  init_image(nx+2, ny+2, image);
 
   // Call the stencil kernel
   double tic = wtime();
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
   //   stencil(nx, ny, tmp_image, image);
   // }
   for(int t = 0; t < 2 * niters - 1; ++t){
-    newStencil(nx, ny, image, currentRow);
+    newStencil(nx+2, ny+2, image, currentRow);
   }
 
   double toc = wtime();
@@ -55,88 +55,81 @@ int main(int argc, char *argv[]) {
   printf(" runtime: %lf s\n", toc-tic);
   printf("------------------------------------\n");
 
-  output_image(OUTPUT_FILE, nx, ny, image);
+  output_image(OUTPUT_FILE, nx+2, ny+2, image);
   free(image);
 }
 
-//use restrict
-void newStencil(const int nx, const int ny, double* restrict image, double* restrict currentRow){
-  double toSetTop;
-  for (int j = 0; j < ny; j++) {
-    int i = 0;
-    toSetTop = image[j+i*ny];
-    // UPDATE 1 : 3.0 / 5 -> 0.6
-    image[j+i*ny] = image[j+i*ny] * 0.6;
-    if (i < nx-1) image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
-    if (j > 0)    image[j+i*ny] += currentRow[j - 1] * 0.1;
-    if (j < ny-1) image[j+i*ny] += image[j+1+i*ny] * 0.1;
-    currentRow[j] = toSetTop;
-  }
-
-  // int previousLeft = image[0];
-  // for (int i = 1; i < nx - 1; i++) {
-  //   int j = 0;
-  //   int prevLeft = image[j + i*ny];
-  //   image[j+i*ny] = image[j+i*ny] * 0.6;
-  //   image[j+i*ny] += previousLeft * 0.1;
-  //   image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
-  //   image[j+i*ny] += image[j+1+i*ny] * 0.1;
-  //   previousLeft = prevLeft;
-  // }
-  //
-  // int previousRight = image[ny - 1];
-  // for(int i = 1; i < nx - 1; i++){
-  //   int j = ny-1;
-  //   int prevRight = image[j+i*ny];
-  //   image[j+i*ny] = image[j+i*ny] * 0.6;
-  //   image[j+i*ny] += previousRight * 0.1;
-  //   image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
-  //   image[j+i*ny] += currentRow[j - 1] * 0.1;
-  //   previousRight = prevRight;
-  // }
-
-  for (int i = 1; i < nx-1; i++) {
-    for (int j = 0; j < ny; j++) {
-      toSetTop = image[j+i*ny];
-      // UPDATE 1 : 3.0 / 5 -> 0.6
-      // image[j+i*ny] = image[j+i*ny] * 0.6;
-      // image[j+i*ny] += currentRow[j] * 0.1;
-      // image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
-      image[j+i*ny] = image[j+i*ny] * 0.6 + (currentRow[j] + image[j + (i+1)*ny])*0.1;
-      if (j > 0)    image[j+i*ny] += currentRow[j - 1] * 0.1;
-      if (j < ny-1) image[j+i*ny] += image[j+1+i*ny] * 0.1;
-      currentRow[j] = toSetTop;
-    }
-  }
-
-
-  for (int j = 0; j < ny; j++) {
-    int i = nx-1;
-    toSetTop = image[j+i*ny];
-    // UPDATE 1 : 3.0 / 5 -> 0.6
-    image[j+i*ny] = image[j+i*ny] * 0.6;
-    if (i > 0)    image[j+i*ny] += currentRow[j] * 0.1;
-    if (j > 0)    image[j+i*ny] += currentRow[j - 1] * 0.1;
-    if (j < ny-1) image[j+i*ny] += image[j+1+i*ny] * 0.1;
-    currentRow[j] = toSetTop;
-  }
-}
-
-// void newStencil(const int nx, const int ny, double* image, double* currentRow){
+// void newStencil(const int nx, const int ny, double* restrict image, double* restrict currentRow){
 //   double toSetTop;
-//   for (int i = 0; i < nx; i++) {
+//   for (int j = 0; j < ny; j++) {
+//     int i = 0;
+//     toSetTop = image[j+i*ny];
+//     // UPDATE 1 : 3.0 / 5 -> 0.6
+//     image[j+i*ny] = image[j+i*ny] * 0.6;
+//     if (i < nx-1) image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
+//     if (j > 0)    image[j+i*ny] += currentRow[j - 1] * 0.1;
+//     if (j < ny-1) image[j+i*ny] += image[j+1+i*ny] * 0.1;
+//     currentRow[j] = toSetTop;
+//   }
+//   for (int i = 1; i < nx-1; i++) {
 //     for (int j = 0; j < ny; j++) {
 //       toSetTop = image[j+i*ny];
 //       // UPDATE 1 : 3.0 / 5 -> 0.6
 //       image[j+i*ny] = image[j+i*ny] * 0.6;
-//       if (i > 0)    image[j+i*ny] += currentRow[j] * 0.1;
-//       if (i < nx-1) image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
+//       image[j+i*ny] += currentRow[j] * 0.1;
+//       image[j+i*ny] += image[j  +(i+1)*ny] * 0.1;
 //       if (j > 0)    image[j+i*ny] += currentRow[j - 1] * 0.1;
 //       if (j < ny-1) image[j+i*ny] += image[j+1+i*ny] * 0.1;
 //       currentRow[j] = toSetTop;
 //     }
 //   }
+//
+//   for (int j = 0; j < ny; j++) {
+//     int i = nx-1;
+//     toSetTop = image[j+i*ny];
+//     // UPDATE 1 : 3.0 / 5 -> 0.6
+//     image[j+i*ny] = image[j+i*ny] * 0.6;
+//     if (i > 0)    image[j+i*ny] += currentRow[j] * 0.1;
+//     if (j > 0)    image[j+i*ny] += currentRow[j - 1] * 0.1;
+//     if (j < ny-1) image[j+i*ny] += image[j+1+i*ny] * 0.1;
+//     currentRow[j] = toSetTop;
+//   }
 // }
+
+void newStencil(const int nx, const int ny, double* restrict image, double* restrict currentRow){
+  double toSetTop;
+
+  //DE CE NU FUNCTIONEAZA????
+
+  // toSetTop = image[1+ny];
+  // image[1+ny] = image[1+ny] * 0.6 +
+  //   (image[1+2*ny]
+  //     + image[2+ny]) * 0.1;
+  // currentRow[1] = toSetTop;
+  // for(int j = 2; j < ny-1; j++){
+  //   toSetTop = image[j+ny];
+  //   image[j+ny] = image[j+ny] * 0.6 +
+  //     (image[j+2*ny]
+  //       + currentRow[j-1]
+  //       + image[j+1+ny]) * 0.1;
+  //   currentRow[j] = toSetTop;
+  // }
+  for(int j = 0; j < ny; j++)
+    currentRow[j] = 0.0;
+  for (int i = 1; i < nx-1; i++) {
+    for (int j = 1; j < ny-1; j++) {
+      toSetTop = image[j+i*ny];
+      // UPDATE 1 : 3.0 / 5 -> 0.6
+      image[j+i*ny] = image[j+i*ny] * 0.6 +
+        (currentRow[j]
+          + image[j  +(i+1)*ny]
+          + currentRow[j - 1]
+          + image[j+1+i*ny]) * 0.1;
+      currentRow[j] = toSetTop;
+    }
+  }
+
+}
 
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   // UPDATE 2 : change order of fors
@@ -166,10 +159,10 @@ void init_image(const int nx, const int ny, double * image) {
   // Checkerboard
   for (int j = 0; j < 8; ++j) {
     for (int i = 0; i < 8; ++i) {
-      for (int jj = j*ny/8; jj < (j+1)*ny/8; ++jj) {
-        for (int ii = i*nx/8; ii < (i+1)*nx/8; ++ii) {
+      for (int jj = j*(ny-2)/8; jj < (j+1)*(ny-2)/8; ++jj) {
+        for (int ii = i*(nx-2)/8; ii < (i+1)*(nx-2)/8; ++ii) {
           if ((i+j)%2)
-          image[jj+ii*ny] = 100.0;
+          image[(jj+1)+(ii+1)*ny] = 100.0;
         }
       }
     }
@@ -187,22 +180,21 @@ void output_image(const char * file_name, const int nx, const int ny, double *im
   }
 
   // Ouptut image header
-  fprintf(fp, "P5 %d %d 255\n", nx, ny);
+  fprintf(fp, "P5 %d %d 255\n", nx-2, ny-2);
 
   // Calculate maximum value of image
   // This is used to rescale the values
   // to a range of 0-255 for output
   double maximum = 0.0;
-  for (int j = 0; j < ny; ++j) {
-    for (int i = 0; i < nx; ++i) {
+  for (int j = 1; j < ny-1; ++j) {
+    for (int i = 1; i < nx-1; ++i) {
       if (image[j+i*ny] > maximum)
         maximum = image[j+i*ny];
     }
   }
-
   // Output image, converting to numbers 0-255
-  for (int j = 0; j < ny; ++j) {
-    for (int i = 0; i < nx; ++i) {
+  for (int j = 1; j < ny-1; ++j) {
+    for (int i = 1; i < nx-1; ++i) {
       fputc((char)(255.0*image[j+i*ny]/maximum), fp);
     }
   }
